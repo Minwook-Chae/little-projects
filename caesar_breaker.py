150a58d13d38652c7b4d10538efb1dcc2ca0a320
mchae <mchae772@gmail.com>
2022-04-30 03:15:50 -0500

    Update caesar_breaker.py

--- caesar_breaker.py
+++ caesar_breaker.py
@@ -1,3 +1,18 @@
 print("Enter Caesar Cipher to break.")
 message = input('> ') 
 
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+for key in range(len(SYMBOLS)):
+    translated = ''
+
+    for symbol in message:
+        if symbol in SYMBOLS:
+            num = SYMBOLS.find(symbol)
+            num = num - key
+            if num < 0:
+                num = num + len(SYMBOLS)
+            translated = translated + SYMBOLS[num]
+        else:
+            translated = translated + symbol
+    print(f"Key #{key}: {translated}")
